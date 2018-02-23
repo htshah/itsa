b7619832d6fda7db794be2efd56cb806f28be921
Het Shah <htshah60@gmail.com>
2018-02-23 13:30:53 +0530

    Created practicals tab

--- index.php
+++ index.php
@@ -50,6 +50,7 @@
 					<li><a href="#objectives">Objectives</a></li>
 					<li><a href="#team">Team</a></li>
 					<li><a href="#about">About</a></li>
+					<li><a href="/practicals.php">Practicals</a></li>
 				</ul>
 
 				<ul id="nav-mobile" class="left side-nav">
