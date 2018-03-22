6f23b9b2f821665cd908357ba086b9b110642093
Het Shah <htshah60@gmail.com>
2018-03-22 22:28:55 +0530

    Email fixes

--- email.php
+++ email.php
@@ -1,5 +1,5 @@
 <?php
-	$msg= printf("Hello <b>%s %s</b>,\n Thank you for registering with itsa.",$_GET['fname'],$_GET['lname']);
+	$msg= sprintf("Hello <b>%s %s</b>,\n Thank you for registering with itsa.",$_GET['fname'],$_GET['lname']);
 	mail($_GET['email'],'Registration successful',$msg);
 
 	die(json_encode(['success'=>1]));
